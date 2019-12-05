03a8f561e4855b53e54f586a3cda1d9d40bc73db
Monica Perez <monicap.@Monicas-MacBook-Pro.local>
2019-12-04 20:15:14 -0500

    We added requirements, connected to database, created a login form in html

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 # from routes.py import *
-
 
 app = Flask (__name__)
 app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://url_name:Sky08880@localhost:8889/Restaurant_waitlist'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLALchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+if__name__== "__main__":
+app.run()
 
 
 
--- routes.py
+++ routes.py
@@ -1,6 +1,12 @@
 
 from flask import Flask
 
+
+
+# @app.route('/')
+#     def index():
+#         return render_template('index.html')
+
 #route for reservation\
 
 @app.route("/reservation/add")

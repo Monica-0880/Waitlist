d085b928ab0863f900fc1bfe6ae88e4ea51bb96d
Monica Perez <monicap.@Monicas-MacBook-Pro.local>
2019-12-05 20:13:56 -0500

    website's working and we added resturant model

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask (__name__)
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://url_name:Sky08880@localhost:8889/Restaurant_waitlist'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://url_name:Sky08880@localhost:8889/Restaurant_waitlist'
 app.config['SQLALCHEMY_ECHO'] = True
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if__name__== "__main__":
-app.run()
+if __name__ == "__main__":
+    app.run()
 
+class Restaurant(db.Model):
+    id = db.Column( db.Integer, primary_key=True )
+    company_name = db.Column( db.String(120))
+    username = db.Column( db.String(120))
+    password = db.Column( db.String(120))
 
-
-    
+    def __init__(self, company_name, username, password):
+        self.company_name = company_name
+        self.username = username
+        self.password = password
+        
